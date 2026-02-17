3606233adee6576cc48b077df91668c6058cb34d
Abby McClain <Armcclain2017@gmail.com>
2026-02-17 14:32:02 -0500

    update tailwind paths
    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
-  theme: { extend: {} },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
-
+}
