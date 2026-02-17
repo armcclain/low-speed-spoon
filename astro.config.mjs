612e63102e149154f7555d16b6d897e46c758c1e
Abby McClain <94204039+armcclain@users.noreply.github.com>
2026-02-17 13:34:49 -0500

    Update astro.config.mjs
    updated to try to fix path issues
    
    Signed-off-by: Abby McClain <94204039+armcclain@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // 1. Set the live URL
   site: 'https://armcclain.github.io',
-  base: 'low-speed-spoon', 
- 
+  // 2. Set the sub-directory (your repo name)
+  base: '/low-speed-spoon',
+  integrations: [tailwind()],
 });
