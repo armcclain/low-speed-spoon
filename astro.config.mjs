17e07ece66d9e44c980a5b348ba3a52d02df150f
Abby McClain <Armcclain2017@gmail.com>
2026-02-17 14:05:20 -0500

    merged
    merged

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   // 1. Set the live URL
   site: 'https://armcclain.github.io',
-  base: 'low-speed-spoon', 
+  base: '/low-speed-spoon', 
  
 });
