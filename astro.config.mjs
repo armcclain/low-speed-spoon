d45c55d094b6376062ddfce46ef4930e322a9b54
Abby McClain <Armcclain2017@gmail.com>
2026-02-17 12:56:03 -0500

    astro.config.mjs update
    updated file to point to lowspeedspoon.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://armcclain.github.io/low-speed-spoon/',
-  base: '/low-speed-spoon/',
-  integrations: [],
+  site: 'https://lowspeedspoon.com',
+  base: '/', 
 });
